6a0402e765c07bdded287259334620d821dc964e
NatashadeLima <lim22010@byui.edu>
2024-11-12 15:34:38 -0300

    Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,43 @@
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Developer";
+        job1._startYear = 2015;
+        job1._endYear = 2020;
+
+        // Console.WriteLine(job1._company);
+
+        // job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+
+        job2._company = "Apple";
+        job2._jobTitle = "Senior Developer";
+        job2._startYear = 2020;
+        job2._endYear = 2023;
+
+        // Console.WriteLine(job2._company);
+
+        // job2.DisplayJobDetails();
+
+        Resume infoResume = new Resume();
+        infoResume._name = "João Amaral";
+
+        infoResume._jobs.Add(job1);
+        infoResume._jobs.Add(job2);
+
+        //Console.WriteLine(infoResume._jobs[0]._jobTitle);
+
+        infoResume.DisplayJobDetails();
+
     }
+
+
 }
