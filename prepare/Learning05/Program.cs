cbe1c0d6eaaaefa2c0bbf99bc040bd5b697f280f
NatashadeLima <lim22010@byui.edu>
2024-12-03 22:31:50 -0300

    Program

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,22 @@
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Console.WriteLine("Testing Assignment Class:");
+        Assignment simpleAssignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(simpleAssignment.GetSummary());
+
+        Console.WriteLine("\nTesting MathAssignment Class:");
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        Console.WriteLine("\nTesting WritingAssignment Class:");
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
---
+++ prepare/Learning05/WritingAssignment.cs
@@ -0,0 +1,17 @@
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    
+    public WritingAssignment(string studentName, string topic, string title)
+        : base(studentName, topic) 
+    {
+        _title = title;
+    }
+
+    
+    public string GetWritingInformation()
+    {
+        return $"The Causes of World War II by {GetStudentName()}";
+    }
+}
